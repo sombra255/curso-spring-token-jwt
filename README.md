c1dd6e6b9a9dbe2743eb114f19e6b334ad3dbb86
Fabricio <fabricio.tidir@gmail.com>
2018-08-28 22:03:20 -0300

    projeto de autenticacao com token jwt finalizado e implementacao do travis para integracao continua do projeto

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
+[![Build Status](https://travis-ci.org/sombra255/curso-spring-token-jwt.svg?branch=master)](https://travis-ci.org/sombra255/curso-spring-token-jwt)
+
 # curso-spring-token-jwt
--- src/main/java/br/com/fabricio/api/security/config/WebSecurityConfig.java
+++ src/main/java/br/com/fabricio/api/security/config/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package br.com.fabricio.api.security.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -50,5 +51,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		httpSecurity.addFilterBefore(authenticationTokenFilterBean(), UsernamePasswordAuthenticationFilter.class);
 		httpSecurity.headers().cacheControl();
 	}
+	
+	@Bean
+    public AuthenticationManager customAuthenticationManager() throws Exception {
+        return authenticationManager();
+    }
 
 }
